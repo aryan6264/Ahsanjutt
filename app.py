cea175fc1cf578dd6bedebb58134cc0ae45aecea
ayanshpandit9 <kartikmishra6803@gmail.com>
2025-07-17 21:23:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def send_comment():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vampire RuLex Comment Bot</title>
+  <title>Vampire RuLex </title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     label {
